226da290eeb2c360287d403af3395fc147d8109f
Jonathan Marques <35399832+jonmarques@users.noreply.github.com>
2023-04-03 23:28:24 -0300

    Update Config.java

--- src/br/com/sauran/matcher/data/Config.java
+++ src/br/com/sauran/matcher/data/Config.java
@@ -3,13 +3,13 @@ package br.com.sauran.matcher.data;
 public class Config {
 
 	private String prefix = "mt.";
-	private String discordListToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NDk5MjA2ODU4OTkxMjA4NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTg3MzM0MDc1fQ.wBxnJm0Pt9CSMmC4cFWUEZWIPhptjmKm_yT9yaTzSo8";
+	private String discordListToken = "";
 
-	private String botToken = "Njk0OTkyMDY4NTg5OTEyMDg0.XtaskQ.XuT-YNm3luWgznpcToOQ-LBAM6g";
+	private String botToken = "";
 	private String botId = "694992068589912084";
 	private boolean test = false;
 
-	//private String botToken = "Njk1MzU0OTMyMTUyNTAwMjQ1.XvjayA.CjuEKfCiUaWThG4Phbo2t5py8dA";
+	//private String botToken = "";
 	//private String botId = "695354932152500245";
 	//private boolean test = true;
 
